be77314c5db28bfb5f95367e74906ec4799a851a
Baimenovbn <baibn@mail.ru>
2022-12-29 18:58:22 +0600

    feat: finish string splitter

--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -8,11 +9,26 @@ public class StringSplitter {
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        StringBuilder regex = new StringBuilder();
+
+        regex.append('[');
+        for (String delimiter : delimiters) {
+            regex.append(delimiter);
+        }
+        regex.append(']');
+
+        ArrayList<String> splitByDelimiters = new ArrayList<>();
+
+        for (String s : source.split(regex.toString())) {
+            if (!s.isEmpty()) {
+                splitByDelimiters.add(s);
+            }
+        }
+        return splitByDelimiters;
     }
 }
